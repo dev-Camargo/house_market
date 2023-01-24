c932b991bfd614d8c74aeee806f9a569a1d16fc1
dev-Camargo <rscamargo@gmail.com>
2023-01-24 13:32:47 -0300

    24_01_23

--- house_market/items.py
+++ house_market/items.py
@@ -2,17 +2,5 @@ import scrapy
 from scrapy.loader.processors import MapCompose, TakeFirst, Join
 
 
-def square_root(ch):
-    return ch.replace("m", 'm'+format('\u00B2'))
-
-
 class HouseMarketItem(scrapy.Item):
-    link = scrapy.Field(output_processor=TakeFirst())
-    price = scrapy.Field(output_processor=TakeFirst())
-    address = scrapy.Field(output_processor=TakeFirst())
-    type = scrapy.Field(output_processor=TakeFirst())
-    area = scrapy.Field(input_processor=MapCompose(
-        square_root), output_processor=TakeFirst())
-    bathroom = scrapy.Field(output_processor=TakeFirst())
-    bedroom = scrapy.Field(output_processor=TakeFirst())
     pass
--- house_market/spiders/remax.py
+++ house_market/spiders/remax.py
@@ -10,8 +10,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from scrapy.selector import Selector
 from time import sleep
-from house_market.items import HouseMarketItem
-from scrapy.loader import ItemLoader
 
 
 def iniciar_driver():
@@ -19,6 +17,7 @@ def iniciar_driver():
     LOGGER.setLevel(logging.WARNING)
     arguments = ['--lang=pt-BR', '--window-size=1920,1080',
                  '--headless', '--disable-gpu', '--no-sandbox']
+
     for argument in arguments:
         chrome_options.add_argument(argument)
 
@@ -48,7 +47,7 @@ class RemaxBotSpider(scrapy.Spider):
     name = 'remaxbot'
 
     def start_requests(self):
-        urls = ['https://remax.pt/comprar?searchQueryState=%7B"regionName":"","sort":%7B"fieldToSort":"ContractDate","order":1%7D,"businessType":1,"listingClass":1,"page":1,"t":"","mapIsOpen":false,"mapScroll":false,"searchNextToMe":false,"listingTypes":%5B%5D%7D']
+        urls = ['https://remax.pt/comprar?searchQueryState=%7B"regionName":"Lisboa","sort":%7B"fieldToSort":"ContractDate","order":1%7D,"businessType":1,"listingClass":1,"page":1,"t":"","mapIsOpen":false,"mapScroll":false,"searchNextToMe":false,"listingTypes":%5B"11"%5D,"price":%7B"min":null,"max":100000%7D,"prn":"Lisboa","regionID":"76","regionType":"Region1ID","regionCoordinates":%7B"latitude":38.8404598668955,"longitude":-9.2186952991834%7D,"regionZoom":9%7D']
 
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse, meta={'next_url': urls[0]})
@@ -56,47 +55,37 @@ class RemaxBotSpider(scrapy.Spider):
     def parse(self, response):
         driver, wait = iniciar_driver()
         driver.get(response.meta["next_url"])
-        response_webdriver = Selector(text=driver.page_source)
 
-        for find in response_webdriver.xpath("//div[@class='col-12 col-sm-6 col-md-6 col-lg-4 col-xl-3 result']"):
-            loader = ItemLoader(item=HouseMarketItem(),
-                                selector=find, response=response)
+        page_arrow_path = "//li[@class='arrow page-item'][1]/a"
 
-            loader.add_xpath(
-                "link", ".//div[@class='listing-search-searchdetails-component']/a/@href")
-            loader.add_xpath(
-                "price", ".//div[@class='figCaption']/p[@class='listing-price']/text()")
-            loader.add_xpath(
-                "address", ".//div[@class='listing-body']/h2[@class='listing-address']/span/text()")
-            loader.add_xpath(
-                "type", ".//ul[@class='listing-footer']/li[@class='listing-type']/text()")
-            loader.add_xpath(
-                "area", ".//li[@class='listing-area']/i/following-sibling::text()")
-            loader.add_xpath(
-                "bathroom", ".//li[@class='listing-bathroom']/i/following-sibling::text()")
-            loader.add_xpath(
-                "bedroom", ".//li[@class='listing-bedroom']/i/following-sibling::text()")
+        while True:
+            results = driver.find_elements(
+                By.XPATH, "//div[@class='col-12 col-sm-6 col-md-6 col-lg-4 col-xl-3 result']")
 
-            yield loader.load_item()
+            for result in results:
+                yield {
+                    "link": result.find_element(By.XPATH, ".//div[@class='listing-search-searchdetails-component']/a").get_attribute('href'),
+                    "image": result.find_element(By.XPATH, ".//div/div[@class='listing-picture']/figure/picture/img").get_attribute('src'),
+                    "price": result.find_element(By.XPATH, ".//div[@class='figCaption']/p[@class='listing-price']").text,
+                    "address": result.find_element(By.XPATH, ".//div[@class='listing-body']/h2[@class='listing-address']/span").text,
+                    "type": result.find_element(By.XPATH, ".//ul[@class='listing-footer']/li[@class='listing-type']").text,
+                    "area": (result.find_element(By.XPATH, ".//li[@class='listing-area']").text).replace("\n2", format('\u00B2')),
+                    "bathroom": result.find_element(By.XPATH, ".//li[@class='listing-bathroom']").text,
+                    "bedroom": result.find_element(By.XPATH, ".//li[@class='listing-bedroom']").text,
+                }
 
-        # results = driver.find_elements(
-        #     By.XPATH, "//div[@class='col-12 col-sm-6 col-md-6 col-lg-4 col-xl-3 result']")
+            try:
+                next_page_arrow = driver.find_element(
+                    By.XPATH, page_arrow_path)
+            except NoSuchElementException:
+                break
 
-        # for result in results:
-        #     yield {
-        #         "link": result.find_element(By.XPATH, ".//div[@class='listing-search-searchdetails-component']/a").get_attribute('href'),
-        #         "image": result.find_element(By.XPATH, ".//div/div[@class='listing-picture']/figure/picture/source[@media='(min-width: 376px) and (max-width: 750px)']").get_attribute('srcset'),
-        #         "price": result.find_element(By.XPATH, ".//div[@class='figCaption']/p[@class='listing-price']").text,
-        #         "address": result.find_element(By.XPATH, ".//div[@class='listing-body']/h2[@class='listing-address']/span").text,
-        #         "type": result.find_element(By.XPATH, ".//ul[@class='listing-footer']/li[@class='listing-type']").text,
-        #         "area": result.find_element(By.XPATH, ".//li[@class='listing-area']").text,
-        #         "bathroom": result.find_element(By.XPATH, ".//li[@class='listing-bathroom']").text,
-        #         "bedroom": result.find_element(By.XPATH, ".//li[@class='listing-bedroom']").text
-        #     }
+            driver.execute_script(
+                'arguments[0].click()', next_page_arrow)
 
-        # if "xpath".is_enabled(): clica
-        # else sai do loop
-        # except:
+            page_arrow_path = "//li[@class='arrow page-item'][2]/a"
+
+            sleep(2)
 
         driver.close()
 
@@ -104,3 +93,11 @@ class RemaxBotSpider(scrapy.Spider):
 def get_remax_url():
     # Selenium apply available filters - return optimized url
     pass
+
+
+def check_if_exists(xpath, driver):
+    try:
+        driver.find_element_by_xpath(xpath)
+    except NoSuchElementException:
+        return False
+    return True
